4f06889d0ac74c6add46c79394046fa228b60777
Addey Welch <awelch@basecampcodingacademy.org>
2017-01-17 15:38:46 -0600

    Set debug to False, add Heroku to allow host

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', "dev-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['addey-portfolio.herokuapp.com']
 
 # Application definition
 
